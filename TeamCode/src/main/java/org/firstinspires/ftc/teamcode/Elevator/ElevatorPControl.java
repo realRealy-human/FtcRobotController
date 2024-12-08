98c6e6a479691b87f60b94b44e7a412b67c83fcd
Lior2011 <prodigyembershedlegend@gmail.com>
2024-12-08 16:47:18 +0200

    kP code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Elevator/ElevatorPControl.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.Elevator;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class ElevatorPControl {
+    private DcMotor motor;
+    private Telemetry telemetry;
+    private double kp =  0.18; // Proportional gain
+
+    public ElevatorPControl(HardwareMap hardwareMap, Telemetry telemetry) {
+        motor = hardwareMap.get(DcMotor.class, "leftMotor");
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        this.telemetry = telemetry;
+    }
+
+    public void goTo(double target) {
+        double error = target - motor.getCurrentPosition()  /28f * 12f / 120f * 12.5f;
+        double output = kp * error;
+
+        motor.setPower(output);
+        telemetry.addData("leftMotor position" ,motor.getCurrentPosition()  /28f * 12f / 120f * 12.5f);
+        telemetry.update();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/firstAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/firstAuto.java
@@ -9,32 +9,15 @@ import org.firstinspires.ftc.teamcode.Intake.Claw;
 
 @TeleOp(name = "firstAuto", group = "Examples")
 public class firstAuto extends LinearOpMode {
-    private double kp =  0.18; // Proportional gain
     private DcMotor motor;
     private  double target;
 
     @Override
     public void runOpMode() {
-        motor = hardwareMap.get(DcMotor.class, "leftMotor");
 
-        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        double error = 0;
         waitForStart();
 
         while (opModeIsActive()) {
-            if (gamepad1.a) { target = 0;}
-            else if (gamepad1.b) {target = 40;}
-            else if (gamepad1.x) {target = 100;}
-            else if ( gamepad1.y) {target = 80;}
-            error = target - motor.getCurrentPosition()  /28f * 12f / 120f * 12.5f;
-            double output = kp * error;
-
-            motor.setPower(output);
-           telemetry.addData("leftMotor position" ,motor.getCurrentPosition()  /28f * 12f / 120f * 12.5f);
-           telemetry.update();
         }
-
-
     }
 }
