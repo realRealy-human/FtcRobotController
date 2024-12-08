6c690563a19f8cd85a08d79842c8ea2b6256b9fa
Lior2011 <prodigyembershedlegend@gmail.com>
2024-12-08 16:41:30 +0200

    kP code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/firstAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/firstAuto.java
@@ -4,22 +4,37 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Intake.Claw;
 
 @TeleOp(name = "firstAuto", group = "Examples")
 public class firstAuto extends LinearOpMode {
-    private Claw claw;
+    private double kp =  0.18; // Proportional gain
+    private DcMotor motor;
+    private  double target;
 
     @Override
     public void runOpMode() {
-        claw = new Claw(hardwareMap, telemetry);
+        motor = hardwareMap.get(DcMotor.class, "leftMotor");
 
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        double error = 0;
         waitForStart();
 
         while (opModeIsActive()) {
-            claw.openOrCloseClaw(gamepad1.a, gamepad1.y);
+            if (gamepad1.a) { target = 0;}
+            else if (gamepad1.b) {target = 40;}
+            else if (gamepad1.x) {target = 100;}
+            else if ( gamepad1.y) {target = 80;}
+            error = target - motor.getCurrentPosition()  /28f * 12f / 120f * 12.5f;
+            double output = kp * error;
 
-            sleep(200);
+            motor.setPower(output);
+           telemetry.addData("leftMotor position" ,motor.getCurrentPosition()  /28f * 12f / 120f * 12.5f);
+           telemetry.update();
         }
+
+
     }
 }
