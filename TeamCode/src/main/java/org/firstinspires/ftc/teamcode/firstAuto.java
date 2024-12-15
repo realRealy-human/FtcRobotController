48e332b138b81fa4d3a55e0fa03e413d9b050b92
Lior2011 <prodigyembershedlegend@gmail.com>
2024-12-15 16:24:58 +0200

    searchAndCollectGamePeice

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/firstAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/firstAuto.java
@@ -6,9 +6,11 @@ import org.firstinspires.ftc.teamcode.Elevator.ElevatorPControl;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.Elevator.ElevatorPControl;
+import org.firstinspires.ftc.teamcode.Intake.IntakeAndArm;
 
 @TeleOp(name = "firstAuto", group = "Examples")
 public class firstAuto extends LinearOpMode {
+    private IntakeAndArm intakeAndArm;
     private Basket basket;
     private ElevatorPControl elevatorPControl;
 
@@ -18,6 +20,7 @@ public class firstAuto extends LinearOpMode {
       public void runOpMode() {
         basket = new Basket(hardwareMap, telemetry, "servo1");
         elevatorPControl = new ElevatorPControl(hardwareMap, telemetry);
+        intakeAndArm = new IntakeAndArm(hardwareMap, telemetry);
 
         basket.closeBasket();
 
@@ -26,15 +29,18 @@ public class firstAuto extends LinearOpMode {
         boolean previousAState = false;
 
         while (opModeIsActive()) {
-            if (gamepad1.a) {
-                elevatorPControl.goTo(0);
-                if (elevatorPControl.FindLocation() < 500){
-                    elevatorPControl.goTo(1000);
-                    basket.openBasketLimited(1000);
-                }
-                else {
-                    elevatorPControl.goTo(0);
-                }
+//            if (gamepad1.a) {
+//                elevatorPControl.goTo(0);
+//                if (elevatorPControl.FindLocation() < 500){
+//                    elevatorPControl.goTo(1000);
+//                    basket.openBasketLimited(1000);
+//                }
+//                else {
+//                    elevatorPControl.goTo(0);
+//                }
+//            }
+            if (gamepad1.y) {
+                intakeAndArm.searchAndCollectGamePiece(true);
             }
         }
     }  }
