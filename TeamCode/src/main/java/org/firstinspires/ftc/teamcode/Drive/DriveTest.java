42e8b52d8235eab3cd655012380537844f9929ac
realRealy-human <saharbarshem@gmail.com>
2024-12-19 16:45:33 +0200

    Auto stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive/DriveTest.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.Drive;
+
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class DriveTest {
+    private DcMotor leftFront;
+    private DcMotor rightFront;
+    private DcMotor leftRear;
+    private DcMotor rightRear;
+    private IMU imu;
+    private Telemetry telemetry;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomus/AutoBasketOne.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.autonomus;
+
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+@Autonomous(name = "AutoBasketOne", group = "Examples")
+public class AutoBasketOne extends LinearOpMode {
+    private org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive drive;
+    private Pose2d startPose = new Pose2d( 0, -72, 0);
+
+    @Override
+    public void runOpMode() {
+        drive = new SampleMecanumDrive(hardwareMap);
+        drive.setPoseEstimate(startPose);
+
+        TrajectorySequence seq1 = drive.trajectorySequenceBuilder(startPose)
+                .lineToConstantHeading(new Vector2d(0, 24))
+                .lineToConstantHeading(new Vector2d(0, MeterToInch(1.287)))
+                .back(0.05)
+                .turn(Math.toDegrees(90))
+                .back(0.9144)
+                .turn(Math.toDegrees(Math.atan(0.37161216 * 1.5) / (0.37161216 * 0.5)))
+                .forward(MeterToInch(Math.sqrt(0.37161216 * 0.5) / (0.37161216 * 1.5)))
+                .turn(Math.toDegrees(180 - Math.atan((0.37161216 * 0.5) / (0.37161216 * 1.5))))
+                .back(0.37161216 * 1.5)
+                .forward(0.37161216 * 2)
+//                .turn(Math.toDegrees(-22))
+//                .back(1)
+                .forward(1)
+
+
+
+
+
+                .build();
+
+        waitForStart();
+
+        if (!isStopRequested()) {
+            drive.followTrajectorySequence(seq1);
+        }
+
+
+    }
+
+    private double MeterToInch(double Meter) {
+        return Meter * 39.3701;
+    }
+
+
+}
