4ef1a83f2317e49baacb034f8a6d9ce262a560e3
realRealy-human <saharbarshem@gmail.com>
2024-12-22 15:43:09 +0200

    Auto stuff road runner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomus/AutoBasketOne.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomus/AutoBasketOne.java
@@ -6,6 +6,16 @@ import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.teamcode.Arm;
+import org.firstinspires.ftc.teamcode.Basket;
+import org.firstinspires.ftc.teamcode.Elevator.ElevatorControl;
+import org.firstinspires.ftc.teamcode.Elevator.ElevatorPControl;
+import org.firstinspires.ftc.teamcode.Elevator.ElevatorAndClaw;
+import org.firstinspires.ftc.teamcode.Intake.Intake1;
+import org.firstinspires.ftc.teamcode.Intake.Claw;
+import org.firstinspires.ftc.teamcode.Color;
+
+
 
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
@@ -14,29 +24,65 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 @Autonomous(name = "AutoBasketOne", group = "Examples")
 public class AutoBasketOne extends LinearOpMode {
     private org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive drive;
-    private Pose2d startPose = new Pose2d( 0, -72, 0);
+    private Pose2d startPose = new Pose2d( 0, -72, 0); // 1
+    private Arm arm;
+    private ElevatorPControl Elevator;
+    private Claw Claw;
+    private Intake1 Intake;
+    private Basket Basket;
+
 
     @Override
     public void runOpMode() {
+        arm = new Arm(hardwareMap, telemetry, "");
+
         drive = new SampleMecanumDrive(hardwareMap);
         drive.setPoseEstimate(startPose);
 
         TrajectorySequence seq1 = drive.trajectorySequenceBuilder(startPose)
-                .lineToConstantHeading(new Vector2d(0, -16))
-                .lineToConstantHeading(new Vector2d(0, MeterToInch(1.287)))
-                .splineToLinearHeading(new Pose2d(-52, -36, Math.toRadians(180)), Math.toRadians(0))
-                .lineToLinearHeading(new Pose2d(-60,-60,Math.toRadians(225)))
-                .turn(Math.toDegrees(Math.atan(0.37161216 * 1.5) / (0.37161216 * 0.5)))
-                .forward(MeterToInch(Math.sqrt(0.37161216 * 0.5) / (0.37161216 * 1.5)))
-                .turn(Math.toDegrees(180 - Math.atan((0.37161216 * 0.5) / (0.37161216 * 1.5))))
-                .back(0.37161216 * 1.5)
-                .forward(0.37161216 * 2)
-//                .turn(Math.toDegrees(-22))
-//                .back(1)
-                .forward(1)
+                .lineToConstantHeading(new Vector2d(0, -18)) // 2
+                .addTemporalMarker(() -> Elevator.goTo(1000))
+                .lineToConstantHeading(new Vector2d(0, -16)) // 2
+                .addTemporalMarker(() -> Elevator.goTo(700))
+                .addTemporalMarker(() -> Claw.openOrCloseClaw(true,false))
+                .lineToConstantHeading(new Vector2d(0, -18))
+                .addTemporalMarker(() -> Elevator.goTo(0))
+                .addTemporalMarker(() -> Claw.openOrCloseClaw(false,true))
+                .splineToLinearHeading(new Pose2d(52, -36, Math.toRadians(180)), Math.toRadians(0)) // 3
+                .addTemporalMarker(() -> Intake.waitForGamePiece())
 
-
-
+                .lineToLinearHeading(new Pose2d(60,-60,Math.toRadians(225))) // 4
+                .addTemporalMarker(() -> arm.moveArmP(0))
+                .addTemporalMarker(() -> Intake.move(1))
+                .wait(20)
+                .addTemporalMarker(() -> Elevator.goTo(1000))
+                .addTemporalMarker(() -> Basket.openBasket())
+                .addTemporalMarker(() -> Elevator.goTo(0))
+                .addTemporalMarker(() -> Basket.closeBasket())
+                .addTemporalMarker(() -> arm.moveArmP(100))
+                .lineToLinearHeading(new Pose2d(60,-36, Math.toRadians(180))) // 5
+                .addTemporalMarker(() -> Intake.waitForGamePiece())
+                .lineToLinearHeading(new Pose2d(60,-60,Math.toRadians(225))) // 6
+                .addTemporalMarker(() -> arm.moveArmP(0))
+                .addTemporalMarker(() -> Intake.move(1))
+                .wait(20)
+                .addTemporalMarker(() ->Elevator.goTo(1000))
+                .addTemporalMarker(() -> Basket.openBasket())
+                .addTemporalMarker(() -> Elevator.goTo(0))
+                .addTemporalMarker(() -> Basket.closeBasket())
+                .addTemporalMarker(() -> arm.moveArmP(100))
+                .lineToLinearHeading(new Pose2d(60,-36,Math.toRadians(45)))
+                .addTemporalMarker(() -> Intake.waitForGamePiece())
+                .lineToLinearHeading(new Pose2d(60,-60,Math.toRadians(45))) // 8
+                .addTemporalMarker(() -> arm.moveArmP(0))
+                .addTemporalMarker(() -> Intake.move(1))
+                .wait(20)
+                .addTemporalMarker(() -> Elevator.goTo(1000))
+                .addTemporalMarker(() -> Basket.openBasket())
+                .addTemporalMarker(() -> Elevator.goTo(0))
+                .addTemporalMarker(() -> Basket.closeBasket())
+                .addTemporalMarker(() -> arm.moveArmP(100))
+                .splineToLinearHeading(new Pose2d(-16, 0, Math.toRadians(180)), Math.toRadians(0)) //9
 
 
                 .build();
